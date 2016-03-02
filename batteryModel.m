229cf3962c30bf95f061c083921a74dfa1aec10a
Eclipse <eclipse.ets@gmail.com>
2016-03-02 16:01:43 -0500

    Firt commit

---
+++ batteryModel.m
@@ -0,0 +1,74 @@
+%% Éclipse 9
+%  Modèle du batterie pack
+%  
+%  Les paramètres proviennent de la datasheet suivante :
+%   https://www.akkuteile.de/tpl/download/NCR-18650BF.pdf
+%
+%  Auteur : Julien Longchamp
+%  Date de création : 26-02-2016
+%  Dernière modification :
+%%
+
+nb_cell_serie = 38;
+nb_cell_para = 11;
+nb_cell_total = nb_cell_serie*nb_cell_para;
+
+Ecell_max = 4.2;    % V
+Ecell_min = 2.6;    % V
+Ccell = 3.35;       % Ah
+Crate_max = 2;
+Rcell = 0.125;      % ohm (NRC18560B from http://lygte-info.dk/review/batteries2012/Common18650Summary%20UK.html) 
+
+Ebatt_max = Ecell_max*nb_cell_serie;
+Ibatt_max = Crate_max*Ccell*nb_cell_para;
+
+Battery_capacity = Ccell*nb_cell_total;
+
+% Points provenant de la courbe de décharge caractéristique
+discharge_0p2C = [0 4.2;
+                .10 4.1;
+                .50 4.0;
+               1.00 3.85;
+               1.50 3.7;
+               2.00 3.6;
+               2.50 3.45;
+               3.00 3.3;
+               3.35 2.5;];
+           
+discharge_1C =   [0 4.0;
+                .10 3.9;
+                .50 3.8;
+               1.00 3.7;
+               1.50 3.5;
+               2.00 3.4;
+               2.50 3.3;
+               3.00 3.1;
+               3.35 2.5;];
+           
+discharge_2C =   [0 3.75;
+                .10 3.7;
+                .50 3.6;
+               1.00 3.5;
+               1.50 3.35;
+               2.00 3.25;
+               2.50 3.1;
+               3.00 2.8;
+               3.35 2.5;];
+           
+continous_capacity = linspace(0,3.35,1000);
+p1 = polyfit(discharge_0p2C(:,1), discharge_0p2C(:,2), 7);
+p2 = polyfit(discharge_1C(:,1), discharge_1C(:,2), 7);
+p3 = polyfit(discharge_2C(:,1), discharge_2C(:,2), 7);
+continous_discharge_0p2C = polyval(p1, continous_capacity);
+continous_discharge_1C = polyval(p2, continous_capacity);
+continous_discharge_2C = polyval(p3, continous_capacity);
+
+figure, hold on
+plot(discharge_0p2C(:,1), discharge_0p2C(:,2), 'sr')
+plot(continous_capacity,continous_discharge_0p2C, 'r')
+plot(discharge_1C(:,1), discharge_1C(:,2), 'sb')
+plot(continous_capacity,continous_discharge_1C, 'b')
+plot(discharge_2C(:,1), discharge_2C(:,2), 'sg')
+plot(continous_capacity,continous_discharge_2C, 'g')
+
+
---
+++ main_simulateur.m
@@ -0,0 +1,89 @@
+%% Éclipse 9
+%  Simulateur de la dynamique du véhicule
+%  
+%  Auteur : Julien Longchamp
+%  Date de création : 24-02-2016
+%%
+
+clc
+clear all
+close all
+
+%% IMPORTATION DES DONNÉES GPS PROVENANT DE FICHIERS CSV
+% % Charge un parcours dont le format est [Type Latitude Longitude Altitude(m) Distance(km) Interval(m)]
+% etape1 = importGPSfromCSV('donnees_gps\ASC2016_etape1.csv');
+% etape2 = importGPSfromCSV('donnees_gps\ASC2016_etape2.csv');
+% etape3 = importGPSfromCSV('donnees_gps\ASC2016_etape3.csv');
+% etape4 = importGPSfromCSV('donnees_gps\ASC2016_etape4.csv');
+% 
+% % Sauvegarde des données en format .mat
+% save('etapesASC2016.mat', 'etape1', 'etape2', 'etape3', 'etape4');
+% % FIN IMPORTATION DES DONNÉES GPS PROVENANT DE FICHIERS CSV
+
+%% IMPORTATION DES DONNÉES EN FORMAT .MAT DE L'ASC2016
+load('etapesASC2016.mat')
+
+% Concaténation des 4 étapes pour constituer le parcours complet
+trajet_complet.type = [etape1.type; etape2.type; etape3.type; etape4.type];
+trajet_complet.latitude = [etape1.latitude; etape2.latitude; etape3.latitude; etape4.latitude];
+trajet_complet.longitude = [etape1.longitude; etape2.longitude; etape3.longitude; etape4.longitude];
+trajet_complet.altitude = [etape1.altitude; etape2.altitude; etape3.altitude; etape4.altitude];
+trajet_complet.slope = [etape1.slope; etape2.slope; etape3.slope; etape4.slope];
+trajet_complet.distance = [etape1.distance; etape1.distance(end)+etape2.distance; etape1.distance(end)+etape2.distance(end)+etape3.distance; etape1.distance(end)+etape2.distance(end)+etape3.distance(end)+etape4.distance];
+trajet_complet.distance_interval = [etape1.distance_interval; etape2.distance_interval; etape3.distance_interval; etape4.distance_interval];
+
+parcours = etape1;
+
+% Prétraitement de la pente (parcours.slope) à l'aide d'un FIR rectangulaire
+for k=1:length(parcours.slope)
+    parcours.slope(k) = mean(parcours.slope(max([1 k-100]):k));
+    parcours.altitude(k) = mean(parcours.altitude(max([1 k-10]):k));
+end
+deniveles = [0; diff(parcours.altitude)];
+pente = 100*deniveles./parcours.distance_interval;
+for k=1:length(pente)
+    pente(k) = mean(pente(max([1 k-20]):k));
+end
+ascention_tot = sum(deniveles.*(sign(deniveles)+1)/2)
+descente_tot= sum(deniveles.*(sign(deniveles)-1)/-2)
+
+figure, hold on, title('Carte 2D du parcours');
+plot(parcours.longitude, parcours.latitude);
+
+figure, hold on,  title('Carte 3D du parcours');
+plot3(parcours.longitude, parcours.latitude, parcours.altitude);
+
+figure, hold on,  title('Élévation du parcours');
+subplot(2,1,1), plot(parcours.distance, parcours.altitude)
+ylabel('Altitude (m)')
+subplot(2,1,2), plot(parcours.distance, parcours.slope)
+hold on, plot(parcours.distance, pente, 'r')
+ylabel('Pente (%)')
+xlabel('Distance (km)')
+
+
+
+
+% Paramètres du véhicule Éclipse 9
+masse_totale = 225;     % kg
+aire_frontale = 1.14;   % m^2
+coef_trainee = 0.135;   % coefficient de trainée aérodynamique
+rayon_roue = 0.2575;    % m
+surface_solaire = 6;    % m^2
+
+% Constantes physiques
+const_grav = 9.81;      % m/s^2
+densite_air = 1.15;     % kg/m^3
+
+% Paramètres des moteurs
+nb_moteur = 2;
+vitesse_nom = 111;  % rad/s
+couple_nom = 16.2;  % Nm
+Kv = 0.45;          % V/rad/s (Constante EMF)
+Ka = 0.44;          % Nm/A (Constante de couple)
+puissance_max = 1800; % W
+
+
+
+
+
